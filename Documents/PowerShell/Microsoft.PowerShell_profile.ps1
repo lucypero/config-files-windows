82bfda60b9cc6b9bcf82bb3fec46561cec3d47c7
lucy <lucyperopero@gmail.com>
2022-07-20 15:12:57 -0300

    make ps config pretty

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,27 +3,55 @@ Set-PSReadlineOption -EditMode vi
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
+### Aliases
+
+Set-Alias ech EditHelixConfig
 function EditHelixConfig {hx $env:appdata\helix\config.toml}
-function EditBookmarks {hx ~\docs\bookmarks}
+
+Set-Alias ecp EditPowershellProfile
 function EditPowershellProfile {hx $profile}
+
+Set-Alias f GoToBookmark
 function GoToBookmark {
     $dir = Get-Content ~\docs\bookmarks | fzf
     Set-Location $dir
 }
+
+Set-Alias e hx
+
+Set-Alias ba AddToBookmarks
 function AddToBookmarks {
     $dir = Get-Location
     Add-Content ~\docs\bookmarks $dir
 }
+
+Set-Alias eb EditBookmarks
+function EditBookmarks {hx ~\docs\bookmarks}
+
+# launch godot editor (project picker)
+Set-Alias ge LaunchGodotEditor
 function LaunchGodotEditor {
+    Start-Process  C:\Users\Lucy\dev\lone_wolf_tech\godots\godot-3.4\bin\godot.windows.opt.tools.64.exe -ArgumentList "-p"
+}
+
+# launch godot game on cwd
+Set-Alias geh LaunchGodotGame
+function LaunchGodotGame {
     Start-Process  C:\Users\Lucy\dev\lone_wolf_tech\godots\godot-3.4\bin\godot.windows.opt.tools.64.exe
 }
+
+Set-Alias lp LaunchFavoriteProgram
 function LaunchFavoriteProgram {
     $program = Get-Content ~\docs\fav-programs | fzf
     Start-Process $program
 }
+
+Set-Alias ex CallExplorer
 function CallExplorer {
     explorer .
 }
+
+Set-Alias es OpenHelixSession
 function OpenHelixSession {
     Set-Location ~\docs\sessions
     $the_session = cat (Get-ChildItem ~\docs\sessions | Select -Exp Name | fzf)
@@ -32,35 +60,31 @@ function OpenHelixSession {
     $hx_args = (($the_session -split '\r?\n').Trim() | Select-Object -Skip 2) -Join ' '
     Start-Process -FilePath hx -ArgumentList "$hx_args" -NoNewWindow -Wait
 }
+
+Set-Alias en OpenNotesSession
 function OpenNotesSession {
     Set-Location ~\docs\sessions
     $the_session = cat ~\docs\sessions\notes
     $hx_args = (($the_session -split '\r?\n').Trim() | Select-Object -Skip 2) -Join ' '
     Start-Process -FilePath hx -ArgumentList "$hx_args" -NoNewWindow -Wait
 }
+
+Set-Alias config ConfigGit
 function ConfigGit {
     git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args
 }
+
+Set-Alias lg lazygit
+
+Set-Alias clg LazyGitConfig
 function LazyGitConfig {
     Set-Location
     lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME
 }
 
-Set-Alias ech EditHelixConfig
-Set-Alias ecp EditPowershellProfile
-Set-Alias f GoToBookmark
-Set-Alias e hx
-Set-Alias ba AddToBookmarks
-Set-Alias eb EditBookmarks
-Set-Alias ge LaunchGodotEditor
-Set-Alias lp LaunchFavoriteProgram
-Set-Alias ex CallExplorer
-Set-Alias es OpenHelixSession
-Set-Alias en OpenNotesSession
-Set-Alias config ConfigGit
-Set-Alias lg lazygit
-Set-Alias clg LazyGitConfig
+### Keybinds
 
+# C-O: calls lfcd
 Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
     lfcd
     [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
